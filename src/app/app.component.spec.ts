2ecd90b3d71f81f75b7c248352e11a0b84654c53
Martin Choraine <martin.choraine@gmail.com>
2021-12-13 14:22:24 +0100

    lab 1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'formation-angular-avance-tp-decembre'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('formation-angular-avance-tp-decembre');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('formation-angular-avance-tp-decembre app is running!');
-  });
 });
